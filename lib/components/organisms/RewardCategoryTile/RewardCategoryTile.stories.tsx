7e2dfa02855b9681cb91b3da7e5a5d74ff619dc6
Graeme Houston <graeme.houston@whitelabel-loyalty.com>
2024-08-19 15:27:12 +0100

    Add the base RewardCategory Til with fake data for dev purposes

--- lib/components/organisms/RewardCategoryTile/RewardCategoryTile.stories.tsx
+++ lib/components/organisms/RewardCategoryTile/RewardCategoryTile.stories.tsx
@@ -1,15 +1,60 @@
-import React from 'react';
-import { StoryFn, Meta } from '@storybook/react';
-import RewardCategoryTile from './index';
+import { Meta, StoryFn } from "@storybook/react";
+import React from "react";
+import RewardCategoryTile from "./index";
 
 export default {
-  title: 'components/organisms/RewardCategoryTile',
+  title: "components/organisms/RewardCategoryTile",
   component: RewardCategoryTile,
 } as Meta;
 
-const Template: StoryFn = (args) => <RewardCategoryTile {...args} />;
+const Template: StoryFn<typeof RewardCategoryTile> = (args) => (
+  <RewardCategoryTile {...args} />
+);
 
 export const Default = Template.bind({});
 Default.args = {
-  // Add default props here
+  rewardData: {
+    allowDecorationOverlay: true,
+    id: "9cb2cc08-e033-4200-86a7-95a03ba09462",
+    createdAt: "2018-06-08T08:57:47.198Z",
+    updatedAt: "2018-06-08T08:57:47.198Z",
+    name: "Restaurants",
+    description: "This is an example description",
+    priority: 1,
+    parent: "9cb2cc08-e033-4200-86a7-95a03ba09462",
+    pictureUrl:
+      "https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_1280.jpg",
+  },
+};
+
+export const LongCategoryName = Template.bind({});
+LongCategoryName.args = {
+  rewardData: {
+    allowDecorationOverlay: true,
+    id: "9cb2cc08-e033-4200-86a7-95a03ba09462",
+    createdAt: "2018-06-08T08:57:47.198Z",
+    updatedAt: "2018-06-08T08:57:47.198Z",
+    name: "This is s really really really long title that should overflow its parent.",
+    description: "This is an example description",
+    priority: 1,
+    parent: "9cb2cc08-e033-4200-86a7-95a03ba09462",
+    pictureUrl:
+      "https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_1280.jpg",
+  },
+};
+
+export const NoDecorationOverlay = Template.bind({});
+NoDecorationOverlay.args = {
+  rewardData: {
+    allowDecorationOverlay: false,
+    id: "9cb2cc08-e033-4200-86a7-95a03ba09462",
+    createdAt: "2018-06-08T08:57:47.198Z",
+    updatedAt: "2018-06-08T08:57:47.198Z",
+    name: "Restaurants",
+    description: "This is an example description",
+    priority: 1,
+    parent: "9cb2cc08-e033-4200-86a7-95a03ba09462",
+    pictureUrl:
+      "https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_1280.jpg",
+  },
 };
--- lib/components/organisms/RewardCategoryTile/index.tsx
+++ lib/components/organisms/RewardCategoryTile/index.tsx
@@ -1,16 +1,98 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useTheme } from "@/context/ThemeContext";
+import { sizes } from "@/utils/styling";
+import React from "react";
+import {
+  Alert,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 type RewardCategoryTileProps = {
-  // Define your props here
-}
+  rewardData: {
+    allowDecorationOverlay: boolean;
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    name: string;
+    description: string;
+    priority: number;
+    parent: string;
+    pictureUrl: string;
+  };
+};
 
-const RewardCategoryTile: React.FC<RewardCategoryTileProps> = () => {
+const RewardCategoryTile: React.FC<RewardCategoryTileProps> = ({
+  rewardData,
+}) => {
+  const { theme } = useTheme();
+  const { allowDecorationOverlay } = rewardData;
+
   return (
-    <View>
-      {/* Your component code here */}
-    </View>
+    <TouchableOpacity
+      style={[
+        styles.container,
+        {
+          borderColor: theme.derivedBackground,
+        },
+      ]}
+      onPress={() => Alert.alert("You Pressed, the reward category tile")}
+    >
+      {allowDecorationOverlay && (
+        <View style={[styles.overlay, { backgroundColor: theme.primary }]}>
+          <Text
+            style={[styles.overlayText, { color: theme.primaryText }]}
+            ellipsizeMode="tail"
+            numberOfLines={1}
+          >
+            {rewardData.name}
+          </Text>
+        </View>
+      )}
+      <Image
+        source={{ uri: rewardData.pictureUrl }}
+        style={styles.image}
+        resizeMode="cover"
+        onError={(error) => console.error("Image loading error:", error)}
+      />
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    maxWidth: 275,
+    aspectRatio: 1,
+    borderRadius: sizes.borderRadius,
+    borderWidth: 1,
+    flex: 1,
+    justifyContent: "center",
+    position: "relative",
+    overflow: "hidden",
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+    height: 35,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  overlayText: {
+    fontSize: 16,
+    paddingHorizontal: 30,
+  },
+  image: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+  },
+});
 
 export default RewardCategoryTile;
