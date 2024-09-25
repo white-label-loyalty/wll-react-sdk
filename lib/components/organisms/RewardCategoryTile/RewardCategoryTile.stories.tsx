876429f4c2fa5baf6084e77d5c6f0c27e33a55a9
Graeme Houston <hello@iamgrae.me>
2024-09-25 17:06:03 +0100

    Fix import path

--- lib/components/organisms/RewardCategoryTile/RewardCategoryTile.stories.tsx
+++ lib/components/organisms/RewardCategoryTile/RewardCategoryTile.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryFn } from '@storybook/react';
 import React from 'react';
-import { TileHeight } from 'types/tile';
+import { TileHeight } from '../../../types/tile';
 import RewardCategoryTile from './index';
 
 export default {
