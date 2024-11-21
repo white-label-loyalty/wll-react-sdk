d53df6ad85a4fe0a2eb5a4cbea0c6662c81cc24a
Graeme Houston <hello@iamgrae.me>
2024-11-21 21:38:29 +0000

    fix(tiles): fix content tile body property

--- lib/types/tile.ts
+++ lib/types/tile.ts
@@ -52,7 +52,7 @@ export class PointsTileConfig {
 
 export class ContentTileConfig {
   title?: string | null;
-  description?: string | null;
+  body?: string | null;
   artworkUrl?: string | null;
   ctaLinkTarget?: CTALinkTarget;
   ctaLink?: string | null;
