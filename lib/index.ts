eb8c6965cadd99f4b4b0d01a2f7de435502e6c5a
Graeme Houston <hello@iamgrae.me>
2024-11-26 20:33:02 +0000

    fix(group): export types

--- lib/index.ts
+++ lib/index.ts
@@ -7,6 +7,8 @@ export * from './context/WllSdkContext';
 
 // type exports
 export * from './types/common';
+export * from './types/group';
+export * from './types/navigation';
 export * from './types/section';
 export * from './types/theme';
 export * from './types/tile';
