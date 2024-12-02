3d3a965f3b173c2b9bc0b70d4ab048aa293b52dd
Graeme Houston <hello@iamgrae.me>
2024-12-02 18:21:40 +0000

    refactor: (BannerTile) Extract Banner Tile Components

---
+++ lib/components/organisms/BannerTile/banner-tile-description.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import { useWllSdk } from '../../../context/WllSdkContext';
+import { BannerTileConfig } from '../../../types/tile';
+import { Text } from '../../atoms';
+import { useBannerContext } from '../../atoms/BaseBanner';
+import { useBannerTileStyles } from './styles';
+
+export const BannerTileDescription: FC = () => {
+  const styles = useBannerTileStyles();
+  const { configuration } = useBannerContext();
+  const { description } = configuration as BannerTileConfig;
+  const { theme } = useWllSdk();
+
+  if (!description) return null;
+  return (
+    <Text
+      style={[
+        styles.description,
+        {
+          color: theme.derivedSurfaceText[20],
+        },
+      ]}
+    >
+      {description}
+    </Text>
+  );
+};
---
+++ lib/components/organisms/BannerTile/banner-tile-media.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View } from 'react-native';
+import { BannerTileConfig } from '../../../types/tile';
+import { ProgressiveImage } from '../../atoms';
+import { useBannerContext } from '../../atoms/BaseBanner';
+import { useBannerTileStyles } from './styles';
+
+type BannerTileMediaProps = {
+  isArtworkOnly: boolean;
+};
+
+export const BannerTileMedia: React.FC<BannerTileMediaProps> = ({
+  isArtworkOnly,
+}) => {
+  const styles = useBannerTileStyles();
+  const { configuration } = useBannerContext();
+  const { artworkUrl } = configuration as BannerTileConfig;
+
+  if (!artworkUrl) return null;
+
+  const containerStyle = {
+    width: isArtworkOnly ? '100%' : '30%',
+  };
+
+  return (
+    // @ts-ignore: We are using percentage values for width, which is valid in React Native but TypeScript expects a number.
+    <View style={[styles.mediaContainer, containerStyle]}>
+      <ProgressiveImage source={{ uri: artworkUrl }} style={styles.media} />
+    </View>
+  );
+};
