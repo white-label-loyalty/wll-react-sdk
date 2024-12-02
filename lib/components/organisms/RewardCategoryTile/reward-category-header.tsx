8469669fea74c4a6194845eeac73c38f99836d4a
Graeme Houston <hello@iamgrae.me>
2024-12-02 17:04:36 +0000

    refactor: (RewardCategoryTile) Extract Reward CategoryTile Components

---
+++ lib/components/organisms/RewardCategoryTile/reward-category-header.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react';
+import { View } from 'react-native';
+import { useWllSdk } from '../../../context/WllSdkContext';
+import { RewardCategoryTileConfig } from '../../../types/tile';
+import { Text } from '../../atoms';
+import { useTileContext } from '../../atoms/BaseTile';
+import { useRewardCategoryTileStyles } from './styles';
+
+export const RewardCategoryHeader: FC = () => {
+  const styles = useRewardCategoryTileStyles();
+  const { theme } = useWllSdk();
+  const { configuration } = useTileContext();
+  const { showName, name } = configuration as RewardCategoryTileConfig;
+
+  if (!showName || !name) return null;
+
+  return (
+    <View style={[styles.header, { backgroundColor: theme.primary }]}>
+      <Text
+        style={[styles.headerText, { color: theme.primaryText }]}
+        ellipsizeMode="tail"
+        numberOfLines={1}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
