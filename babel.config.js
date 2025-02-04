40d5cfbd49a81ced589a9b6253683b4bce95a8b7
Graeme Houston <hello@iamgrae.me>
2025-02-04 17:15:59 +0000

    fix: babel in react native, web tags transform to mobile equivalents

--- babel.config.js
+++ babel.config.js
@@ -1,32 +1,46 @@
-module.exports = api => {
+module.exports = (api) => {
   const isTest = api.env('test');
-  
+
   return {
-    presets: [
-      ['module:metro-react-native-babel-preset', { modules: false }]
-    ],
+    presets: [['module:metro-react-native-babel-preset', { modules: false }]],
     plugins: [
-      ['module-resolver', {
-        alias: {
-          '^react-native$': 'react-native-web'
-        }
-      }]
+      [
+        'module-resolver',
+        {
+          alias: {
+            '^react-native$': 'react-native',
+            '^react-dom$': 'react-native',
+            '^react-native-web$': 'react-native',
+          },
+          extensions: [
+            '.ios.js',
+            '.android.js',
+            '.native.js',
+            '.js',
+            '.ts',
+            '.tsx',
+          ],
+        },
+      ],
     ],
     env: {
       test: {
         presets: [
-          ['@babel/preset-env', { targets: { node: 'current' }, modules: 'commonjs' }],
+          [
+            '@babel/preset-env',
+            { targets: { node: 'current' }, modules: 'commonjs' },
+          ],
           '@babel/preset-typescript',
-          ['@babel/preset-react', { runtime: 'automatic' }]
-        ]
+          ['@babel/preset-react', { runtime: 'automatic' }],
+        ],
       },
       production: {
         presets: [
           ['@babel/preset-env', { modules: false }],
           '@babel/preset-typescript',
-          ['@babel/preset-react', { runtime: 'automatic' }]
-        ]
-      }
-    }
+          ['@babel/preset-react', { runtime: 'automatic' }],
+        ],
+      },
+    },
   };
 };
