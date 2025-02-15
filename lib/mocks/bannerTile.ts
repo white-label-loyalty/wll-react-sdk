5a56e08770e022a2b8c3c8cf6a70305ff59d71bf
Graeme Houston <hello@iamgrae.me>
2025-02-15 13:11:35 +0000

    test: (BannerTile) create banner tile mock factory

---
+++ lib/mocks/bannerTile.ts
@@ -0,0 +1,42 @@
+import {
+  BannerTileConfig,
+  CTALinkTarget,
+  TileHeight,
+  TileType,
+} from '../types/tile';
+
+export type BannerTileMockConfig = Partial<BannerTileConfig> & {
+  active?: boolean;
+  tileHeight?: TileHeight;
+};
+
+export const createBannerTileMock = (config?: BannerTileMockConfig) => {
+  const {
+    active = true,
+    tileHeight = TileHeight.Full,
+    title = '',
+    description = '',
+    ctaText = '',
+    ctaLink = '',
+    ctaLinkTarget = CTALinkTarget.sameWindow,
+    artworkUrl = 'https://images.pexels.com/photos/6250883/pexels-photo-6250883.jpeg',
+  } = config || {};
+
+  return {
+    id: '3',
+    type: TileType.Banner,
+    active,
+    createdAt: '',
+    updatedAt: '',
+    tileHeight,
+    priority: 1,
+    configuration: {
+      title,
+      description,
+      ctaText,
+      ctaLink,
+      ctaLinkTarget,
+      artworkUrl,
+    },
+  };
+};
