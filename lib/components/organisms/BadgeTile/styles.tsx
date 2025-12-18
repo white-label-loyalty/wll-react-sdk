f8c56b5b4e78da481074537b29fd1ee085225b0b
george-eduarddinu <george-eduarddinu@whitelabel-loyalty.com>
2025-12-18 17:53:49 +0200

    fix: adjust images

--- lib/components/organisms/BadgeTile/styles.tsx
+++ lib/components/organisms/BadgeTile/styles.tsx
@@ -45,6 +45,7 @@ export const useBadgeTileStyles = (): ReturnType<typeof StyleSheet.create> => {
     },
     header: {
       width: '100%',
+      flexBasis: '50%',
       alignItems: 'center',
       justifyContent: 'center',
       position: 'relative',
